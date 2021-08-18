48903366ab22ad965c4e610c79108db07f2f0d4c
Oliver Christensen <orc13a@live.dk>
2021-08-18 10:51:27 +0200

    array

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    public static int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
     public static void main(String[] args) {
-        System.out.println("Hej");
+        System.out.println(arr.toString());
     }
 }
